7741d9e0a33c55fa0f12c2360aa90e4325c7778b
felixDev22 <felixnzomo@gmail.com>
2024-03-15 23:25:12 +0300

    Updates App. add Routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import './global.css';
+import { Routes, Route } from 'react-router-dom';
+
+import SigninForm from '../_auth/form/SigninForm';
+import Home from '../_root/Home';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        <Route path="/sign-in" element={<SigninForm />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </main>
+  );
 };
 
 export default App;
